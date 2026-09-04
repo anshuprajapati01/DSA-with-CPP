afc9fb56eedeadc5c80874657b50a91d9fbcc2ba
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-09-04 11:07:52 +0530

    Completed Chapter-02 - Cpp-Basics

---
+++ Module-00-CPP-Basics/Chapter-02-Cpp-Basics/01_Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 21;
+    string College_name = "Buddha Institute of Technology";
+    int semester = 5;
+    int marks = 90;
+    cout << "age= " << age << " " << "Colege_name= " << College_name << " " << "Semester= " << semester << " " << "Marks= " << marks;
+    return 0;
+}
