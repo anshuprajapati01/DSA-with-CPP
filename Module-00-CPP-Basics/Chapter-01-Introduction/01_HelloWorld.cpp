d769cd47a8546de523f97b7c9c6b9eb12e9ec1d2
Anshu Prajapati <anshuparajapti61@gmail.com>
2026-09-04 10:46:47 +0530

    Completed Chapter-01 - Hello world

--- Module-00-CPP-Basics/Chapter-01-Introduction/01_HelloWorld.cpp
+++ Module-00-CPP-Basics/Chapter-01-Introduction/01_HelloWorld.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout<<"Hello_World";
+int main (){
+    cout<<"Hello World"<<endl;
+    cout<<"Hello Anshu" <<" "<<"How are you?" <<" " <<"Welcome to DSA with C++";
     return 0;
 }
